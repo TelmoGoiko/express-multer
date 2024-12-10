37c17461a5ed3b2d9cc7abe820fa4b1684a0ecb4
telmogoiko <tgoicoechea002@ikasle.ehu.eus>
2024-12-10 11:54:20 +0100

    Añadir soporte para carga de archivos y un campo de nombre en el formulario

--- routes/profile.js
+++ routes/profile.js
@@ -1,17 +1,37 @@
 var express = require('express');
 var router = express.Router();
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const multer = require('multer')
+const path = require('path')
+const uploadDir = 'public/uploads/';
+
+// Multer storage konfigurazioa
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'public/uploads/')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, `avatar-${uniqueSuffix}${path.extname(file.originalname)}`)
+    }
+  });
+
+// Multer upload konfigurazioa
+const upload = multer({ 
+    storage: storage,
+    limits: {
+      fileSize: 2 * 1024 * 1024 // 2MB-ko muga
+    },
+  });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.redirect('form.html');
 });
 
 router.post('/', upload.single('avatar'), function (req, res, next) {
     console.log(req.file)
     // req.body will hold the text fields, if there were any
-    res.send("Jasota")
+    res.send(`Zure izena:${req.body.izena}. Fitxategia: ${req.file.filename}`);
 })
 
 
