75f4214c7371b5a465fbc3c17fb81722a3480962
telmogoiko <tgoicoechea002@ikasle.ehu.eus>
2024-12-16 10:19:14 +0100

    Añadir validación de tipo de archivo en la configuración de carga de Multer

--- routes/profile.js
+++ routes/profile.js
@@ -5,21 +5,32 @@ const path = require('path')
 
 // Multer storage konfigurazioa
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'public/uploads/')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, `avatar-${uniqueSuffix}${path.extname(file.originalname)}`)
-    }
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads/')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, `avatar-${uniqueSuffix}${path.extname(file.originalname)}`)
+  }
   });
 
 // Multer upload konfigurazioa
 const upload = multer({ 
-    storage: storage,
-    limits: {
-      fileSize: 2 * 1024 * 1024 // 2MB-ko muga
-    },
+  storage: storage,
+  limits: {
+    fileSize: 2 * 1024 * 1024 // 2MB-ko muga
+  },
+  fileFilter: function (req, file, cb) {
+    const filetypes = /jpeg|jpg|png/;
+    const mimetype = filetypes.test(file.mimetype);
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+    if (mimetype && extname) {
+    return cb(null, true);
+    } else {
+    cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+    }
+  }
   });
 
 /* GET home page. */
